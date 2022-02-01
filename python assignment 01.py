2938fd958cf8008674e85f76e349d49ed7bc34d0
NITIN KUMAR <98701125+Nitin-Tanwar-1610@users.noreply.github.com>
2022-02-01 16:33:53 +0530

    Add files via upload

---
+++ python assignment 01.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## Write a python program to print "Hello Python" ?
+print("Hello Python")
+
+
+# In[9]:
+
+
+## Write a python program to do arithmetical addition and division ?
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+
+# In[8]:
+
+
+## Write a python program to find the area of a triangle ?
+base = int(input("Enter the base: "))
+height = int(input("Enter the height: "))
+area_triangle = (1/2)*base*height
+print("Area of triangle is ", area_triangle)
+
+
+# In[13]:
+
+
+## Write a python program to swap two variables ? 
+a = 3
+b = 7
+temp = a
+a = b
+b = temp
+print("a"+ " =" , a)
+print("b" + " =", b)
+
+
+# In[25]:
+
+
+## Write a python program to generate a random number ?
+import random
+list = [2, 4, 6, 7, 8, 3]
+print(random.choice(list))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
